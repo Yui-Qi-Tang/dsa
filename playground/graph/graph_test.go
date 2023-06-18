43c058a8a31ad4e30da154e0bda660e5438629ee
Yui Qi Tang <yqtang1222@gmail.com>
2023-06-19 05:29:51 +0800

    updated go.mod; created an example for graph with dfs

---
+++ playground/graph/graph_test.go
@@ -0,0 +1,42 @@
+package graph
+
+import (
+	"testing"
+)
+
+func TestDFS(t *testing.T) {
+	// create graph
+	a := &node{val: "A", neighbors: make([]*node, 0)}
+	b := &node{val: "B", neighbors: make([]*node, 0)}
+	c := &node{val: "C", neighbors: make([]*node, 0)}
+	d := &node{val: "D", neighbors: make([]*node, 0)}
+	e := &node{val: "E", neighbors: make([]*node, 0)}
+	f := &node{val: "F", neighbors: make([]*node, 0)}
+
+	a.neighbors = append(a.neighbors, b, d, e)
+	b.neighbors = append(b.neighbors, a, c)
+	c.neighbors = append(c.neighbors, b, e)
+	d.neighbors = append(d.neighbors, a, e)
+	e.neighbors = append(e.neighbors, a, c, d, f)
+	f.neighbors = append(f.neighbors, e)
+
+	// DFS
+	stack := []*node{a} // get started from A
+	a.vistied = true
+	result := ""
+	for len(stack) > 0 {
+		n := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		result += n.val
+		for _, nb := range n.neighbors {
+			if !nb.vistied {
+				nb.vistied = true
+				stack = append(stack, nb)
+			}
+		}
+	}
+	if result != "AEFCDB" {
+		t.Fatalf("it should be 'AEFCDB', but got '%s'", result)
+	}
+	t.Log("passed")
+}
