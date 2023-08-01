e4a112198601e1b51202e474351676ca8283e40d
Yui Qi Tang <yqtang1222@gmail.com>
2023-08-01 21:07:14 +0800

    reviewed leetcode problems

--- problems/leetcode/03_contains_duplicate.go
+++ problems/leetcode/03_contains_duplicate.go
@@ -20,6 +20,20 @@ Output: true
 
 */
 
+func ContainDuplicatev29(nums []int) bool {
+	dup := make(map[int]bool, len(nums))
+
+	for _, num := range nums {
+		if dup[num] {
+			return true
+		}
+
+		dup[num] = true
+	}
+
+	return false
+}
+
 func ContainDuplicatev28(nums []int) bool {
 	dup := make(map[int]bool, len(nums))
 
