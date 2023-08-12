9342a0508703f3ce0106f1471100f796c75a4496
Yui Qi Tang <yqtang1222@gmail.com>
2023-08-12 18:28:58 +0800

    reviewed leetcode prolems

--- problems/leetcode/53_max_subarray_test.go
+++ problems/leetcode/53_max_subarray_test.go
@@ -5,6 +5,7 @@ import "testing"
 func TestMaximumSubarray(t *testing.T) {
 
 	testfuncs := []func([]int) int{
+		MaximumSubarrayv39,
 		MaximumSubarrayv38,
 		MaximumSubarrayv37,
 		MaximumSubarrayv36,
