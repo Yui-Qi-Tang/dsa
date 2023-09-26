7b20db54489ea36925212e69401c2aa9b8244e15
Yui Qi Tang <yqtang1222@gmail.com>
2023-09-26 20:00:52 +0800

    reviewed leetcode problems

--- problems/leetcode/07_find_minimum_in_rotated_array_test.go
+++ problems/leetcode/07_find_minimum_in_rotated_array_test.go
@@ -7,6 +7,7 @@ import (
 func TestFindMinRotatedSortedArray(t *testing.T) {
 
 	testfuncs := []func([]int) int{
+		findMinRotatedSortedArrayv16,
 		findMinRotatedSortedArrayv15,
 		findMinRotatedSortedArrayv14,
 		findMinRotatedSortedArrayv13,
