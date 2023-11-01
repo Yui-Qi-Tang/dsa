7de365ff170cdfd390ae5c4e1dcad8708bddc7b8
Yui Qi Tang <yqtang1222@gmail.com>
2023-11-01 09:16:41 +0800

    reviewed leetcode problems finished problem: 08

--- problems/leetcode/14_longest_common_prefix_test.go
+++ problems/leetcode/14_longest_common_prefix_test.go
@@ -5,6 +5,7 @@ import "testing"
 func TestLongestCommonPrefix(t *testing.T) {
 
 	testfuncs := []func([]string) string{
+		longestCommonPrefixv10,
 		longestCommonPrefixv9,
 		longestCommonPrefixv8,
 		longestCommonPrefixv7,
--- problems/leetcode/268_missing_number_test.go
+++ problems/leetcode/268_missing_number_test.go
@@ -5,6 +5,7 @@ import "testing"
 func TestMissingNumber(t *testing.T) {
 
 	testFunc := []func(nums []int) int{
+		missingNumberv13,
 		missingNumberv12,
 		missingNumberv11,
 		missingNumberv10,
