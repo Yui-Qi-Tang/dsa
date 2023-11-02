87c25cbd5fe61d818de7b2c9a41fcfba9f22866b
Yui Qi Tang <yqtang1222@gmail.com>
2023-11-02 09:43:01 +0800

    amend

--- playground/heap/heap.go
+++ playground/heap/heap.go
@@ -4,6 +4,10 @@ func parent(i int) int { return i / 2 }
 func left(i int) int   { return (2 * i) + 1 }
 func right(i int) int  { return (2 * i) + 2 }
 
+func less(i, j int) bool {
+	return i > j
+}
+
 func push(arr *[]int, v ...int) {
 	for _, x := range v {
 		*arr = append(*arr, x)
@@ -15,10 +19,9 @@ func push(arr *[]int, v ...int) {
 func up(arr []int, idx int) {
 	for idx > 0 {
 		p := parent(idx)
-		if arr[p] <= arr[idx] {
+		if !less(arr[p], arr[idx]) {
 			return
 		}
-		// swap if arr[parent] > arr[idx] (min-heapify)
 		arr[p], arr[idx] = arr[idx], arr[p]
 		idx = p
 	}
@@ -33,6 +36,7 @@ func pop(arr *[]int) int {
 	old[0], old[len(old)-1] = old[len(old)-1], old[0]
 	v := old[len(old)-1]
 	*arr = old[:len(old)-1]
+	// why down if pop?
 	down(*arr, 0)
 	return v
 }
@@ -44,16 +48,16 @@ func down(arr []int, idx int) {
 		r := right(curr)
 
 		// handle min heapify (root, left child)
-		if l < len(arr) && arr[curr] > arr[l] {
+		if l < len(arr) && less(arr[curr], arr[l]) {
 			curr = l
 		}
 
 		// handle min heapify (root, right child)
-		if r < len(arr) && arr[curr] > arr[r] {
+		if r < len(arr) && less(arr[curr], arr[r]) {
 			curr = r
 		}
 
-		// return if arr[curr] statisfies min heapify
+		// return if arr[curr] statisfies the heapify
 		if curr == idx {
 			return
 		}
